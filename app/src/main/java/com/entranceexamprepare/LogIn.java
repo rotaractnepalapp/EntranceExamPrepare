af6707d999e0d291f39c1e0aa3b616a53f490ff8
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-02-08 16:54:13 +0545

    firebase database check message "hellow"

--- app/src/main/java/com/entranceexamprepare/LogIn.java
+++ app/src/main/java/com/entranceexamprepare/LogIn.java
@@ -3,12 +3,20 @@ package com.entranceexamprepare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
 
     }
 }
